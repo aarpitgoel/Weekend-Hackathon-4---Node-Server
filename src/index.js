8cea4f7581ff682ac63d70dc54453e156398ab90
aarpitgoel <64373216+aarpitgoel@users.noreply.github.com>
2020-11-14 03:09:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,24 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
-  
+  if(req.url === "./welcome"){
+    res.status(200).write('Welcome to Dominos!'); 
+    res.end();
+  }
+  if(req.url === "./contact"){
+     const respo = {
+          phone: '18602100000',
+          email: 'guestcaredominos@jublfood.com'
+     }
+     res.status(200).send(respo); 
+    res.end();
+  }
+  if(req.url === './'){
+        res.status(404);
+        res.send();
+  }
 }
+
+httpServer.listen(8081);
 
 module.exports = httpServer;
